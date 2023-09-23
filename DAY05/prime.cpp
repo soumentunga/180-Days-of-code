f73006b715ffd06b9872f9e292988bc504d9b4b8
Soumen Tunga <cse22023@cemk.ac.in>
2023-09-23 19:28:34 +0530

    change some code of Prime Number

--- DAY05/prime.cpp
+++ DAY05/prime.cpp
@@ -5,9 +5,9 @@ int main()
     int n,i;
     cout<<"Enter the Number:";
     cin>>n;
-    if(n>2)
+    if(n<2)
     {
-        cout<<"Not Prime";
+        cout<<"Not a Prime Number";
         return 0;
     }
     else
@@ -15,10 +15,14 @@ int main()
         for(i=2;i<n;i=i+1)
         {
             if(n%i==0)
-            cout<<"Not Prime";
+            {
+            cout<<"Not a Prime Number";
             return 0;
-
+            }
         }
-        cout<<"Prime";
+        cout<<"Prime Number";
+        return 0;
     }
+
+    return 0;//to tell the compiler that program is end no need to go next line.
 }
